504eb2d3f537296e12195a558c7224b1a61e8861
daichan132 <wakadai0319@gmail.com>
2023-05-29 19:20:40 +0900

    chatを変更

--- frontend/src/pages/api/openai/chat.ts
+++ frontend/src/pages/api/openai/chat.ts
@@ -7,16 +7,12 @@ import { PromptTemplate } from 'langchain';
 import { HumanChatMessage, AIChatMessage } from 'langchain/schema';
 import { OpenAI } from 'langchain/llms/openai';
 
-const CONDENSE_PROMPT = `以下の会話とフォローアップ質問が与えられた場合、それらを単独の命令文に変換してください。
-会話: {chat_history}
-フォローアップ質問: {question}
+const QA_PROMPT = `あなたは役に立つAIアシスタントです。以下では会話履歴と質問が渡されます。最後の質問に答えてください。
 
-単独の命令文:`;
+会話履歴：{chat_history}
+質問：{question}
 
-const QA_PROMPT = `あなたは役立つAIアシスタントです。以下の質問にMarkdown形式で答えてください。
-質問: {question}
-
-Markdown形式の役立つ答え:`;
+マークダウン形式での役に立つ回答：`;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { question, history } = req.body;
@@ -30,29 +26,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
       return new AIChatMessage(m.content as string);
     });
-    const condense_memory = new BufferWindowMemory({
+    const memory = new BufferWindowMemory({
       k: 5,
       chatHistory: new ChatMessageHistory(pastMessages),
       memoryKey: 'chat_history',
     });
-    const condense_prompt = new PromptTemplate({
-      inputVariables: ['chat_history', 'question'],
-      template: CONDENSE_PROMPT,
-    });
-    const condense_chain = new LLMChain({
-      llm,
-      prompt: condense_prompt,
-      memory: condense_memory,
-    });
-    const condense_result = await condense_chain.call({ question: question.content });
-
-    console.log(condense_result);
     const qa_prompt = new PromptTemplate({
-      inputVariables: ['question'],
+      inputVariables: ['chat_history', 'question'],
       template: QA_PROMPT,
     });
-    const chain = new LLMChain({ llm, prompt: qa_prompt });
-    const result = await chain.call({ question: condense_result.text });
+    const chain = new LLMChain({
+      llm,
+      prompt: qa_prompt,
+      memory,
+    });
+    const result = await chain.call({ question: question.content });
     console.log(result);
     res.status(200).json({ res: result.text });
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
