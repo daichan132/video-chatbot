c09ba5e7ff640eab9e6ce317d62a136b5f93712b
hfukuoka <hayato.fukuoka.q2@gmail.com>
2023-06-01 23:07:08 +0900

    add video-player

--- frontend/src/pages/c/[id].tsx
+++ frontend/src/pages/c/[id].tsx
@@ -2,8 +2,16 @@
 import { Chatbot, useQueryChat, useQueryMessages } from 'src/features/chatbot';
 import { useRouter } from 'next/router';
 import { createGetLayout } from 'src/components/layout';
+import { SimpleGrid, createStyles } from '@mantine/core';
+import { VideoPlayer } from '@/features/videoPlayer';
+
+const useStyles = createStyles(() => ({
+  viewHeight: { height: '100vh', width: '100%', overflow: 'hidden' },
+}));
 
 const ChatPage = () => {
+  const { classes } = useStyles();
+
   const router = useRouter();
   const { data: currentChat, isLoading: isChatLoading } = useQueryChat(router.query?.id as string);
   const { data: messages, isLoading: isMessagesLoading } = useQueryMessages(
@@ -16,7 +24,12 @@ const ChatPage = () => {
         <div />
       ) : (
         <div key={currentChat.id}>
-          <Chatbot currentChat={currentChat} initialMessages={messages} />
+          <div className={classes.viewHeight}>
+            <SimpleGrid>
+              <VideoPlayer src="/free-video5-sky-cafinet.mp4" />
+              <Chatbot currentChat={currentChat} initialMessages={messages} />
+            </SimpleGrid>
+          </div>
         </div>
       )}
     </div>
