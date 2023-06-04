df920cfa54a6e7fbb9019947804e448ea7868a12
hfukuoka <hayato.fukuoka.q2@gmail.com>
2023-06-04 10:41:00 +0900

    fix ui

--- src/features/profile/components/UserButton/UserButton.tsx
+++ src/features/profile/components/UserButton/UserButton.tsx
@@ -1,6 +1,5 @@
-import { UnstyledButton, UnstyledButtonProps, Group, Avatar, Text, Flex } from '@mantine/core';
+import { UnstyledButton, UnstyledButtonProps, Avatar, Text, Flex } from '@mantine/core';
 import { forwardRef } from 'react';
-import { BsThreeDotsVertical } from 'react-icons/bs';
 
 interface UserButtonProps extends UnstyledButtonProps {
   image: string;
@@ -9,7 +8,7 @@ interface UserButtonProps extends UnstyledButtonProps {
 }
 
 export const UserButton = forwardRef<HTMLButtonElement, UserButtonProps>(function UserButton(
-  { image, name, icon, ...others }: UserButtonProps,
+  { image, name, ...others }: UserButtonProps,
   ref
 ) {
   return (
@@ -29,9 +28,8 @@ export const UserButton = forwardRef<HTMLButtonElement, UserButtonProps>(functio
       })}
       {...others}
     >
-      <Group spacing={6}>
+      <Flex gap="md" justify="flex-start" align="center" direction="row">
         <Avatar src={image} size={28} />
-        {/* <div style={{ flex: 1 }}> */}
         <Flex gap="md" justify="flex-start" align="center" direction="row">
           <Text
             size="sm"
@@ -40,12 +38,8 @@ export const UserButton = forwardRef<HTMLButtonElement, UserButtonProps>(functio
           >
             {name}
           </Text>
-          {icon || <BsThreeDotsVertical size="0.9rem" />}
         </Flex>
-        {/* </div> */}
-
-        {/* {icon || <BsThreeDotsVertical size="0.9rem" />} */}
-      </Group>
+      </Flex>
     </UnstyledButton>
   );
 });
